4b9dbfb566d04a933cefe5202f793e243d0c3a20
Lauren McDonnell <mcdonnell88@gmail.com>
2015-03-26 14:28:02 -0700

    add authController

--- client/controllers.js
+++ client/controllers.js
@@ -1,64 +1,64 @@
 var micControllers = angular.module('micControllers', ['ribbitAudienceRTC', 'ribbitPresenterRTC', 'ngSanitize']);
 
 
-micControllers.controller('AuthControl', ['$scope', 'Auth', function($scope, Auth) {
+// micControllers.controller('AuthControl', ['$scope', 'Auth', function($scope, Auth) {
 
-  $scope.normalLogin = function(username, password) {
+//   $scope.normalLogin = function(username, password) {
 
-    var successCb = function(result) {
-      console.log('made it to successCB of normalLogin controller');
-      $location.url('/main');
-    };
+//     var successCb = function(result) {
+//       console.log('made it to successCB of normalLogin controller');
+//       $location.url('/main');
+//     };
 
-    var errorCb = function(err) {
-      console.error(err);
-      $location.url('/');
-    };
+//     var errorCb = function(err) {
+//       console.error(err);
+//       $location.url('/');
+//     };
 
-    var notifyCb = function(result) {
-      console.log(result);
-    };
+//     var notifyCb = function(result) {
+//       console.log(result);
+//     };
 
-    // console.log(username, password);
+//     // console.log(username, password);
 
-    var authPromise = Auth.normalLogin(username, password);
+//     var authPromise = Auth.normalLogin(username, password);
 
-    // console.log(authPromise);
+//     // console.log(authPromise);
 
-    authPromise.then(function() {
-      console.log('promise has resolved');
-    }).finally(function(){
-      console.log('do something');
-    });
+//     authPromise.then(function() {
+//       console.log('promise has resolved');
+//     }).finally(function(){
+//       console.log('do something');
+//     });
 
-    // authPromise
-    //   .catch(function(err) {
-    //     console.error(err);
-    //   })
-    //   .then(function(result) {
-    //     console.log(result);
-    //   });
-      // successCb, errorCb, notifyCb);
-  };
+//     // authPromise
+//     //   .catch(function(err) {
+//     //     console.error(err);
+//     //   })
+//     //   .then(function(result) {
+//     //     console.log(result);
+//     //   });
+//       // successCb, errorCb, notifyCb);
+//   };
 
-  $scope.signup = function(username, password) {
-    console.log('signing up');
-    Auth.signup(username, password)
-      .catch(function(err) {
-        console.error(err);
-      })
-      .then(function(result) {
-        console.log(result);
-      });
-  };
+//   $scope.signup = function(username, password) {
+//     console.log('signing up');
+//     Auth.signup(username, password)
+//       .catch(function(err) {
+//         console.error(err);
+//       })
+//       .then(function(result) {
+//         console.log(result);
+//       });
+//   };
 
-  $scope.gitLogin = function() {
-    Auth.gitLogin();
-    console.log('gitlogin');
-  };
+//   $scope.gitLogin = function() {
+//     Auth.gitLogin();
+//     console.log('gitlogin');
+//   };
 
 
-}]);
+// }]);
 
 micControllers.controller('MainControl', ['$scope', '$location', 'Room', function($scope, $location, Room) {
   $scope.room;
